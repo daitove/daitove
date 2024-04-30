cb8f9068fba0594f06b62545286eaea0e00b88b6
daitove <saba.gogichaishvili@outlook.com>
2024-05-01 03:49:16 +0400

    format component code

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	export let name: keyof typeof icons;
+  export let name: keyof typeof icons;
 
-	let icons = {
-		ge_flag: {
-			boxWidth: 32,
-			boxHeight: 20,
-			svg: `<path fill="#fff" d="m0 0h30v20H0z" /><path d="m13 0h4v20h-4zM0 8h30v4H0z" /><g id="c"><g id="b"><path id="a" d="m5.7968 1.954a5.4 5.4 0 0 0 1.4064 0 10.4 10.4 0 0 0 0 4.092 5.4 5.4 0 0 0-1.4064 0 10.4 10.4 0 0 0 0-4.092z"/><use transform="rotate(90,6.5,4)" xlink:href="#a" /></g><use x="17" xlink:href="#b" /></g><use y="12" xlink:href="#c" />`
-		}
-	} as const;
+  let icons = {
+    ge_flag: {
+      boxWidth: 32,
+      boxHeight: 20,
+      svg: `<path fill="#fff" d="m0 0h30v20H0z" /><path d="m13 0h4v20h-4zM0 8h30v4H0z" /><g id="c"><g id="b"><path id="a" d="m5.7968 1.954a5.4 5.4 0 0 0 1.4064 0 10.4 10.4 0 0 0 0 4.092 5.4 5.4 0 0 0-1.4064 0 10.4 10.4 0 0 0 0-4.092z"/><use transform="rotate(90,6.5,4)" xlink:href="#a" /></g><use x="17" xlink:href="#b" /></g><use y="12" xlink:href="#c" />`
+    }
+  } as const;
 
-	let displayIcon = icons[name];
+  let displayIcon = icons[name];
 </script>
 
 <svg
-	class={$$props.class}
-	fill="currentColor"
-	viewBox="0 0 {displayIcon.boxWidth} {displayIcon.boxHeight}"
+  class={$$props.class}
+  fill="currentColor"
+  viewBox="0 0 {displayIcon.boxWidth} {displayIcon.boxHeight}"
 >
-	{@html displayIcon.svg}
+  {@html displayIcon.svg}
 </svg>
